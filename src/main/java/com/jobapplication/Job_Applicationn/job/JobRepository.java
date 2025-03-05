a0e3f05524d948208be4166615df9f2a81ed2b6e
Idba1 <moniraislamself@gmail.com>
2025-03-05 20:32:55 +0600

    jpa Repository and jobs persistent

---
+++ src/main/java/com/jobapplication/Job_Applicationn/job/JobRepository.java
@@ -0,0 +1,6 @@
+package com.jobapplication.Job_Applicationn.job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
